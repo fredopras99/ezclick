12aadde6ded2d5f62ad885d826ab0d3d9a32dda9
fredopras99 <114176252+fredopras99@users.noreply.github.com>
2022-09-22 23:28:28 +0700

    Add files via upload
    main program

---
+++ init.php
@@ -0,0 +1,12 @@
+<form method="POST" id="myForm" action="">
+    <input type="text" name="url" value="google.com">
+    <input type="max" name="max" value="9999">
+    <input type="submit">
+    </form>
+
+ <script>
+
+     $(document).ready(function() {
+        $('#myForm').submit;
+     });
+    </script>
